4338719fbddbe5f0bec892d20b1889f5177d7682
dkoukoul <dimitris.koukoulakis@gmail.com>
2016-02-15 19:26:30 +0200

    UTF-8 encoding

--- add.php
+++ add.php
@@ -1,11 +1,13 @@
 <?php
 $mysql_db_hostname = "localhost";
-$mysql_db_user = "root";
-$mysql_db_password = "c0mm0ns";
-$mysql_db_database = "queues";
+$mysql_db_user = "user";
+$mysql_db_password = "";
+$mysql_db_database = "table";
 
 $con = mysqli_connect($mysql_db_hostname, $mysql_db_user, $mysql_db_password,
  $mysql_db_database);
+
+$con->set_charset('utf8');
 
 if (!$con) {
  trigger_error('Could not connect to MySQL: ' . mysqli_connect_error());
